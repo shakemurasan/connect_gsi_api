81014bcb5ca2e66f443517b67e812ee1bc6caa58
Yasuhiro Matsumura <ym.contributor@gmail.com>
2016-11-23 20:47:37 +0900

    specで測地APIに実際アクセスしようとしてしまうところをstub化

--- spec/connect_gsi_api_spec.rb
+++ spec/connect_gsi_api_spec.rb
@@ -6,13 +6,42 @@ describe ConnectGsiApi do
   end
 
   describe '.distance2p' do
+    before do
+      allow(ConnectGsiApi).to receive(:connect_gsi_api).with(api_path, valid_params).and_return(result)
+      allow(ConnectGsiApi).to receive(:connect_gsi_api).with(api_path, invalid_params).and_return(nil)
+    end
+
+    let(:api_path) { "http://vldb.gsi.go.jp/sokuchi/surveycalc/surveycalc/bl2st_calc.pl?" }
+    let(:valid_params) {
+      {
+          outputType: "json",
+          ellipsoid:  "bessel",
+          latitude1:  lat1.to_s,
+          longitude1: lng1.to_s,
+          latitude2:  lat2.to_s,
+          longitude2: "139.706666"
+      }
+    }
+    let(:invalid_params) {
+      {
+          outputType: "json",
+          ellipsoid:  "bessel",
+          latitude1:  lat1.to_s,
+          longitude1: lng1.to_s,
+          latitude2:  lat2.to_s,
+          longitude2: "hogehoge"
+      }
+    }
+    let(:result) { { "OutputData" => { "geoLength" => 3852.099 } } }
+
     let(:lat1) { 35.689166 }
     let(:lng1) { 139.704444 }
     let(:lat2) { 35.654444 }
     let(:lng2) { 139.706666 }
-    subject { ConnectGsiApi.distance2p(lat1, lng2, lat2, lng2) }
+    subject { ConnectGsiApi.distance2p(lat1, lng1, lat2, lng2) }
 
     context 'valid parameter' do
+
       it 'return a distance' do
         is_expected.to eq 3852.099
       end
